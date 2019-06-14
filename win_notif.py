ba70eb544443655d7871657f1e826bc8edd25b95
Yuliya Brechka <juliabrechka@gmail.com>
2019-06-14 09:16:25 +0300

    prettify dict

--- win_notif.py
+++ win_notif.py
@@ -2,8 +2,13 @@ import threading
 
 from plyer import notification
 
-kwargs = {'title': 'Eyes exercises', 'message': "It's time for health care", 'app_name': 'Notification', 'timeout': 10,
-          'app_icon': r'D:\программирование\true github\notification\img\eye3.ico'}
+kwargs = {
+    'title': 'Eyes exercises',
+    'message': "It's time for health care",
+    'app_name': 'Notification',
+    'timeout': 10,
+    'app_icon': r'D:\программирование\true github\notification\img\eye3.ico'
+}
 
 notification.notify(**kwargs)
 
